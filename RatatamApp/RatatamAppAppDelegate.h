f4674258f163a8b3e5adf3285d4fe6db18155e98
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-02-22 12:21:59 +0100

    Initial Commit

---
+++ RatatamApp/RatatamAppAppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  RatatamAppAppDelegate.h
+//  RatatamApp
+//
+//  Created by Christophe Hamerling on 22/02/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RatatamAppAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+    NSPersistentStoreCoordinator *__persistentStoreCoordinator;
+    NSManagedObjectModel *__managedObjectModel;
+    NSManagedObjectContext *__managedObjectContext;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ RatatamApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RatatamApp
+//
+//  Created by Christophe Hamerling on 22/02/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
